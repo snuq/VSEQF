92bd0cf36fbcb222f8bfe24164132fb17bcda2f2
Hudson Barkley <snuq@snuq.com>
2019-08-09 15:25:58 -0700

    Fixed a bug where addon could not be run directly from a text area.  Seems to be due to a bug in Blender, might have to undo this change later?

--- VSEQuickFunctions.py
+++ VSEQuickFunctions.py
@@ -6336,31 +6336,31 @@ class VSEQuickFunctionSettings(bpy.types.AddonPreferences):
 
 class VSEQFTempSettings(object):
     """Substitute for the addon preferences when this script isn't loaded as an addon"""
-    parenting: bpy.props.BoolProperty(
+    parenting = bpy.props.BoolProperty(
         name="Enable Quick Parenting",
         default=True)
-    fades: bpy.props.BoolProperty(
+    fades = bpy.props.BoolProperty(
         name="Enable Quick Fades",
         default=True)
-    list: bpy.props.BoolProperty(
+    list = bpy.props.BoolProperty(
         name="Enable Quick List",
         default=True)
-    proxy: bpy.props.BoolProperty(
+    proxy = bpy.props.BoolProperty(
         name="Enable Quick Proxy",
         default=True)
-    markers: bpy.props.BoolProperty(
+    markers = bpy.props.BoolProperty(
         name="Enable Quick Markers",
         default=True)
-    tags: bpy.props.BoolProperty(
+    tags = bpy.props.BoolProperty(
         name="Enable Quick Tags",
         default=True)
-    cuts: bpy.props.BoolProperty(
+    cuts = bpy.props.BoolProperty(
         name="Enable Quick Cuts",
         default=True)
-    edit: bpy.props.BoolProperty(
+    edit = bpy.props.BoolProperty(
         name="Enable Compact Edit Panel",
         default=True)
-    threepoint: bpy.props.BoolProperty(
+    threepoint = bpy.props.BoolProperty(
         name="Enable Quick Three Point",
         default=True)
 
